ab575da3c9e49a9919be9b680914006de051b9fc
bluelovers <codelovers@users.sourceforge.net>
2010-10-04 02:48:24 +0800

    .

--- Scorpio/helpers/array.php
+++ Scorpio/helpers/array.php
@@ -20,26 +20,32 @@ if (0) {
 class Scorpio_helper_array_Core {
 	protected static $instances = null;
 
+	// 取得構造物件
 	public static function instance($overwrite = false) {
-		if (!self::$instances) {
-			$ref = new ReflectionClass(($overwrite && !in_array($overwrite, array(true, 1), true)) ? $overwrite : 'scoarray');
-			self::$instances = $ref->newInstance();
+		if (!static::$instances) {
+			$ref = new ReflectionClass(($overwrite && !in_array($overwrite, array(true, 1), true)) ? $overwrite:get_called_class());
+			static::$instances = $ref->newInstance();
 		} elseif ($overwrite) {
-			$ref = new ReflectionClass(!in_array($overwrite, array(true, 1), true) ? $overwrite : get_class(self::$instances));
-			self::$instances = $ref->newInstance();
+			$ref = new ReflectionClass(!in_array($overwrite, array(true, 1), true) ? $overwrite:get_called_class());
+			static::$instances = $ref->newInstance();
 		}
 
-		return self::$instances;
+		return static::$instances;
 	}
 
+	// 建立構造
 	function __construct() {
 
 		// make sure self::$instances is newer
-		if (!self::$instances || !in_array(get_class($this), class_parents(self::$instances))) {
-			self::$instances = $this;
+		// 當未建立 static::$instances 時 會以當前 class 作為構造類別
+		// 當已建立 static::$instances 時 如果呼叫的 class 不屬於當前 static::$instances 的父類別時 則會自動取代; 反之則 不做任何動作
+		if (!static::$instances || !in_array(get_called_class(), class_parents(static::$instances))) {
+			static::$instances = $this;
 		}
 
-		return self::$instances;
+//		print_r(array(get_called_class(), class_parents(static ::$instances), class_parents(self ::$instances), class_parents(get_called_class())));
+
+		return static::$instances;
 	}
 
 	static function remove_keys($haystack, $needle) {
@@ -80,6 +86,45 @@ class Scorpio_helper_array_Core {
 
 	function in_array_default ($needle, $haystack, $default = null, $strict = false) {
 		return in_array($needle, $haystack, $strict) ? $needle : ($default === null ? $haystack[0] : $default);
+	}
+
+	/**
+	 * I needed to merge two arrays while keeping the first arrays format and adding the values, if they exist, from the second array.
+	 * I was working with several multidimensional arrays and was using one as the primary structure to insert data into a database.
+	 *
+	 * @return array
+	 * @see http://tw2.php.net/manual/en/function.array-merge.php#89445
+	 */
+	function overlay($a1, $a2) {
+		/*
+		    $table = tableDescription();
+		    // created an array of the table structure
+   			$table = array_overlay($table,$_POST);
+   			// writes the values submitted from the array into the table using array names in the form like <input type="text" name="column[Value]" value="" />
+    	*/
+		foreach($a1 as $k => $v) {
+			if(!array_key_exists($k,$a2)) continue;
+			if(is_array($v) && is_array($a2[$k])){
+				$a1[$k] = array_overlay($v,$a2[$k]);
+			}else{
+				$a1[$k] = $a2[$k];
+			}
+		}
+
+		return $a1;
+	}
+
+	function merge(array $array1,array $array2, $strict = true) {
+		$ret = array();
+
+		if ($strict) {
+			$ret = array_merge($array1, $array2);
+		} else {
+			$ret = $array1 + $array2;
+			$ret = static::overlay($ret, $array2);
+		}
+
+		return $ret;
 	}
 }
 
