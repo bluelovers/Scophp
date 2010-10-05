0e71746db79368340b4bd4e6ee1d9cde044c260c
bluelovers <codelovers@users.sourceforge.net>
2010-10-06 04:19:13 +0800

    .

--- Scorpio/helpers/text.php
+++ Scorpio/helpers/text.php
@@ -444,6 +444,77 @@ class Scorpio_helper_text_Core {
 		return $ip_address;
 	}
 
+	/**
+	 * @see http://tw2.php.net/manual/en/function.urldecode.php#62707
+	 */
+	function code2utf($num){
+		if($num<128)
+			return chr($num);
+		if($num<1024)
+			return chr(($num>>6)+192).chr(($num&63)+128);
+		if($num<32768)
+			return chr(($num>>12)+224).chr((($num>>6)&63)+128)
+				.chr(($num&63)+128);
+		if($num<2097152)
+			return chr(($num>>18)+240).chr((($num>>12)&63)+128)
+				.chr((($num>>6)&63)+128).chr(($num&63)+128);
+
+		return '';
+	}
+
+	/**
+	 * @see http://tw2.php.net/manual/en/function.urldecode.php#62707
+	 */
+	function unescape($strIn, $iconv_to = 'UTF-8') {
+		$strOut = '';
+		$iPos = 0;
+		$len = strlen ($strIn);
+		while ($iPos < $len) {
+			$charAt = substr ($strIn, $iPos, 1);
+			if ($charAt == '%') {
+				$iPos++;
+				$charAt = substr ($strIn, $iPos, 1);
+				if ($charAt == 'u') {
+					// Unicode character
+					$iPos++;
+					$unicodeHexVal = substr ($strIn, $iPos, 4);
+					$unicode = hexdec ($unicodeHexVal);
+					$strOut .= static::code2utf($unicode);
+					$iPos += 4;
+				} else {
+					// Escaped ascii character
+					$hexVal = substr ($strIn, $iPos, 2);
+					if (hexdec($hexVal) > 127) {
+						// Convert to Unicode
+						$strOut .= static::code2utf(hexdec ($hexVal));
+					} else {
+						$strOut .= chr (hexdec ($hexVal));
+					}
+					$iPos += 2;
+				}
+			} else {
+				$strOut .= $charAt;
+				$iPos++;
+			}
+		}
+		if ($iconv_to != "UTF-8") {
+			$strOut = iconv("UTF-8", $iconv_to, $strOut);
+		}
+		return $strOut;
+	}
+
+	/*
+	 * @see http://tw2.php.net/manual/en/function.urldecode.php#29272
+	 * For compatibility of new and old brousers:
+	 *		%xx -> char
+	 *		%u0xxxx -> char
+	 */
+	function unicode_decode($txt) {
+		$txt = ereg_replace('%u0([[:alnum:]]{3})', '&#x\1;',$txt);
+		$txt = ereg_replace('%([[:alnum:]]{2})', '&#x\1;',$txt);
+		return ($txt);
+	}
+
 }
 
 /*
