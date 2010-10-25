f06b932900211eb71d3cfd75d5619764b99b2450
bluelovers <codelovers@users.sourceforge.net>
2010-10-25 16:16:24 +0800

    .

--- Scorpio/libs/Hook.php
+++ Scorpio/libs/Hook.php
@@ -21,7 +21,8 @@ class Scorpio_Hook_Core {
 	protected static $hooklist = array();
 	protected static $calevenlist = array();
 
-	const RET_FAILED = null;
+//	const RET_FAILED = null;
+	const RET_FAILED = false;
 	const RET_SUCCESS = true;
 	const RET_STOP = false;
 
