073b2688b0481bfa2e0ee0d23dba156209e2cc3a
bluelovers <codelovers@users.sourceforge.net>
2011-06-27 14:37:49 +0800

    libs:Event Init.

---
+++ Scorpio/libs/Event.php
@@ -0,0 +1,19 @@
+<?
+
+/**
+ * This Class for call Hook
+ *
+ * @author bluelovers
+ * @copyright 2010
+ */
+
+if (0) {
+	// for IDE
+	class Scorpio_Event extends Scorpio_Event_Core_ {}
+}
+
+class Scorpio_Event_Core_ {
+
+}
+
+?>
