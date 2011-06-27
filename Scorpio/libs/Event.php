7f8abc0446da81c2bf4e6262b2d15df394d311ff
bluelovers <codelovers@users.sourceforge.net>
2011-06-27 14:47:17 +0800

    init method

--- Scorpio/libs/Event.php
+++ Scorpio/libs/Event.php
@@ -13,7 +13,32 @@ if (0) {
 }
 
 class Scorpio_Event_Core_ {
+	static $stop = false;
 
+	/**
+	 * 呼叫點
+	 **/
+	function run() {
+		if (self::$stop) return false;
+	}
+
+	/**
+	 * 暫停呼叫事件
+	 **/
+	function stop() {
+		if (self::$stop) return ;
+
+		self::$stop = true;
+	}
+
+	/**
+	 * 繼續執行事件
+	 **/
+	function play() {
+		if (!self::$stop) return ;
+
+		self::$stop = false;
+	}
 }
 
 ?>
