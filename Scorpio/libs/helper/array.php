034649d7024d83f751263f43216db8e7f5fa29b8
bluelovers <codelovers@users.sourceforge.net>
2011-09-07 11:59:38 +0800

    class Scorpio_helper_array_Core_

---
+++ Scorpio/libs/helper/array.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * @author bluelovers
+ * @copyright 2011
+ */
+
+if (0) {
+	// for IDE
+	class scoarray extends Scorpio_helper_array_Core_ {
+	}
+}
+
+class Scorpio_helper_array_Core_ {
+
+}
+
+?>
