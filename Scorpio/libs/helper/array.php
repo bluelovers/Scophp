cdfebc3f06ba3044f1da3dd12b8fc45663020794
bluelovers <codelovers@users.sourceforge.net>
2011-09-07 12:03:15 +0800

    $a1[$k] = scoarray::overlay($v,$a2[$k]);

--- Scorpio/libs/helper/array.php
+++ Scorpio/libs/helper/array.php
@@ -83,7 +83,7 @@ class Scorpio_helper_array_Core_ {
 		foreach($a1 as $k => $v) {
 			if(!array_key_exists($k,$a2)) continue;
 			if(is_array($v) && is_array($a2[$k])){
-				$a1[$k] = array_overlay($v,$a2[$k]);
+				$a1[$k] = scoarray::overlay($v,$a2[$k]);
 			}else{
 				$a1[$k] = $a2[$k];
 			}
