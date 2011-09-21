f38a09385b86359c450a6b7f8d127626264f29f6
bluelovers <codelovers@users.sourceforge.net>
2011-09-21 22:44:05 +0800

    if (!defined('SCORPIO_SYNTAX') || !empty(SCORPIO_SYNTAX))

--- Scorpio/Bootstrap.php
+++ Scorpio/Bootstrap.php
@@ -15,8 +15,10 @@ include_once SCORPIO_SYSPATH . 'Scorpio/libs/Kenal.php';
 
 Scorpio_Kenal_Core_::_class_setup();
 
+if (!defined('SCORPIO_SYNTAX') || !empty(SCORPIO_SYNTAX)) {
 foreach (Scorpio_File_Core::scandir_ext('php', SCORPIO_SYSPATH.'Scorpio/syntax') as $_file) {
 	include_once SCORPIO_SYSPATH.'Scorpio/syntax/' . $_file;
+}
 }
 
 unset($_file);
