91bd8a1fbb6b57862142c5a42fc8f43b9a2eaf5f
bluelovers <codelovers@users.sourceforge.net>
2011-09-21 23:07:31 +0800

    .

--- Scorpio/libs/helper/file.php
+++ Scorpio/libs/helper/file.php
@@ -14,10 +14,10 @@
 
 if (0) {
 	// for IDE
-	class scofile extends Scorpio_helper_file_Core {}
+	class scofile extends Scorpio_helper_file_Core_ {}
 }
 
-class Scorpio_helper_file_Core {
+class Scorpio_helper_file_Core_ {
 
 	static $temp;
 
