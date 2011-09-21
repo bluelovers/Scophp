08a4ec6317ef9a89af2f5dec43c4b1ed6882abfb
bluelovers <codelovers@users.sourceforge.net>
2011-09-21 22:12:13 +0800

    include_once

--- Scorpio/Bootstrap.php
+++ Scorpio/Bootstrap.php
@@ -6,7 +6,7 @@
  */
 
 if (!defined('SCORPIO_SYSPATH')) {
-	require_once dirname(__FILE__).'/libs/File.php';
+	include_once dirname(__FILE__).'/libs/File.php';
 
 	define('SCORPIO_SYSPATH', Scorpio_File_Core::dirname(__FILE__, '..', 1));
 }
