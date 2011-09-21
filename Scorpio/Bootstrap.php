4b4c99d848c370f8435b1692aa113cb5bc021dc3
bluelovers <codelovers@users.sourceforge.net>
2011-09-21 22:11:43 +0800

    SCORPIO_

--- Scorpio/Bootstrap.php
+++ Scorpio/Bootstrap.php
@@ -5,10 +5,10 @@
  * @copyright 2011
  */
 
-if (!defined('SYSPATH')) {
+if (!defined('SCORPIO_SYSPATH')) {
 	require_once dirname(__FILE__).'/libs/File.php';
 
-	define('SYSPATH', Scorpio_File_Core::dirname(__FILE__, '..', 1));
+	define('SCORPIO_SYSPATH', Scorpio_File_Core::dirname(__FILE__, '..', 1));
 }
 
 ?>
