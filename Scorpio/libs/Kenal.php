0f51532ff6bff3127ec9e50ff22b4cf4eb002622
bluelovers <codelovers@users.sourceforge.net>
2011-09-21 22:38:35 +0800

    .

--- Scorpio/libs/Kenal.php
+++ Scorpio/libs/Kenal.php
@@ -66,8 +66,7 @@ class Scorpio_Kenal_Core_ {
 		$m = array();
 		if (Scorpio_Kenal::_class_loader_by_defined($class)) {
 			$ret = true;
-		} else
-		if (preg_match('/^(?<pre>Scorpio_)(?<class>.+)(?<core>'.$_core_.')?$/', $m)) {
+		} elseif (preg_match('/^(?<pre>Scorpio_)(?<class>.+)(?<core>'.$_core_.')?$/', $m)) {
 			if (!class_exists($m['core'] ? $m[0] : $m['pre'].$m['class'].$_core_, false)) {
 				$paths = split('_', $m['class']);
 				$file = array_pop($paths);
