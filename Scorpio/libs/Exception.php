96bb37d817d49db05aef278cbeb46c018b64c610
bluelovers <codelovers@users.sourceforge.net>
2011-09-23 05:19:28 +0800

    public static function handler(Exception $e)

--- Scorpio/libs/Exception.php
+++ Scorpio/libs/Exception.php
@@ -120,91 +120,117 @@ class Scorpio_Exception_Core_ extends Exception {
 	}
 
 	/**
-	 * exception handler, displays the error message, source of the
+	 * Inline exception handler, displays the error message, source of the
 	 * exception, and the stack trace of the error.
 	 *
-	 * @uses    Kohana::message()
-	 * @uses    Kohana_Exception::text()
+	 * @uses    Kohana_Exception::text
 	 * @param   object   exception object
-	 * @return  void
+	 * @return  boolean
 	 */
-	public static function handle(Exception $e) {
-		try {
+	public static function handler(Exception $e)
+	{
+		try
+		{
 			// Get the exception information
-			$type = get_class($e);
-			$code = $e->getCode();
+			$type    = get_class($e);
+			$code    = $e->getCode();
 			$message = $e->getMessage();
+			$file    = $e->getFile();
+			$line    = $e->getLine();
 
-			// Create a text version of the exception
-			$error = Scorpio_Exception::text($e);
+			// Get the exception backtrace
+			$trace = $e->getTrace();
 
-			// Add this exception to the log
-			Scorpio_Log::add('error', $error);
-
-			// Manually save logs after exceptions
-			Scorpio_Log::save();
-
-			if (Scorpio_Kenal::config('kohana/core.display_errors') === false) {
-				// Do not show the details
-				$file = $line = null;
-				$trace = array();
-
-				$template = '_disabled';
-			} else {
-				$file = $e->getFile();
-				$line = $e->getLine();
-				$trace = $e->getTrace();
-
-				$template = '';
-			}
-
-			if ($e instanceof Scorpio_Exception) {
-				$template = $e->getTemplate() . $template;
-
-				if (!headers_sent()) {
-					$e->sendHeaders();
-				}
-
-				// Use the human-readable error name
-				$code = Scorpio_Kenal::message('kohana/core.errors.' . $code);
-			} else {
-				$template = Scorpio_Exception::$template . $template;
-
-				if (!headers_sent()) {
-					header('HTTP/1.1 500 Internal Server Error');
-				}
-
-				if ($e instanceof ErrorException) {
+			if ($e instanceof ErrorException)
+			{
+				if (isset(Kohana_Exception::$php_errors[$code]))
+				{
 					// Use the human-readable error name
-					$code = Scorpio_Kenal::message('kohana/core.errors.' . $e->getSeverity());
+					$code = Kohana_Exception::$php_errors[$code];
+				}
 
-					if (version_compare(PHP_VERSION, '5.3', '<')) {
-						// Workaround for a bug in ErrorException::getTrace() that exists in
-						// all PHP 5.2 versions. @see http://bugs.php.net/45895
-						for ($i = count($trace) - 1; $i > 0; --$i) {
-							if (isset($trace[$i - 1]['args'])) {
-								// Re-position the arguments
-								$trace[$i]['args'] = $trace[$i - 1]['args'];
+				if (version_compare(PHP_VERSION, '5.3', '<'))
+				{
+					// Workaround for a bug in ErrorException::getTrace() that exists in
+					// all PHP 5.2 versions. @see http://bugs.php.net/bug.php?id=45895
+					for ($i = count($trace) - 1; $i > 0; --$i)
+					{
+						if (isset($trace[$i - 1]['args']))
+						{
+							// Re-position the args
+							$trace[$i]['args'] = $trace[$i - 1]['args'];
 
-								unset($trace[$i - 1]['args']);
-							}
+							// Remove the args
+							unset($trace[$i - 1]['args']);
 						}
 					}
 				}
 			}
 
-			// Clean the output buffer if one exists
-			ob_get_level() and ob_clean();
+			// Create a text version of the exception
+			$error = Kohana_Exception::text($e);
+
+			if (is_object(Kohana::$log))
+			{
+				// Add this exception to the log
+				Kohana::$log->add(Log::ERROR, $error);
+
+				// Make sure the logs are written
+				Kohana::$log->write();
+			}
+
+			if (Kohana::$is_cli)
+			{
+				// Just display the text of the exception
+				echo "\n{$error}\n";
+
+				exit(1);
+			}
+
+			if ( ! headers_sent())
+			{
+				// Make sure the proper http header is sent
+				$http_header_status = ($e instanceof HTTP_Exception) ? $code : 500;
+
+				header('Content-Type: text/html; charset='.Kohana::$charset, TRUE, $http_header_status);
+			}
+
+			if (Request::$current !== NULL AND Request::current()->is_ajax() === TRUE)
+			{
+				// Just display the text of the exception
+				echo "\n{$error}\n";
+
+				exit(1);
+			}
+
+			// Start an output buffer
+			ob_start();
+
+			// Include the exception HTML
+			if ($view_file = Kohana::find_file('views', Kohana_Exception::$error_view))
+			{
+				include $view_file;
+			}
+			else
+			{
+				throw new Kohana_Exception('Error view file does not exist: views/:file', array(
+					':file' => Kohana_Exception::$error_view,
+				));
+			}
+
+			// Display the contents of the output buffer
+			echo ob_get_clean();
+
+			exit(1);
 		}
-		catch (Exception $e) {
+		catch (Exception $e)
+		{
 			// Clean the output buffer if one exists
 			ob_get_level() and ob_clean();
 
 			// Display the exception text
-			echo Scorpio_Exception::text($e), LF;
-		}
+			echo Kohana_Exception::text($e), "\n";
 
-		if (Scorpio_Kenal::$server_api === 'cli') {
 			// Exit with an error status
 			exit(1);
 		}
