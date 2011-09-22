7075e1b7437bb6dd1e999929a3a8c317b29e1386
bluelovers <codelovers@users.sourceforge.net>
2011-09-23 03:26:19 +0800

    Scorpio_Exception::$php_errors[E_DEPRECATED] = 'Deprecated';

--- Scorpio/libs/Exception.php
+++ Scorpio/libs/Exception.php
@@ -50,6 +50,11 @@ class Scorpio_Exception_Core_ extends Exception {
 	public function __construct($message, $variables = null, $code = 0) {
 		$this->instance_identifier = uniqid();
 
+		if (defined('E_DEPRECATED')) {
+			// E_DEPRECATED only exists in PHP >= 5.3.0
+			Scorpio_Exception::$php_errors[E_DEPRECATED] = 'Deprecated';
+		}
+
 		$variables !== null && $message = scotext::sprintf($message, $variables);
 
 		// Sets $this->message the proper way
