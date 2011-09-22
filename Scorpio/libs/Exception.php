46adf268e331424261c9f66818530113fd582b38
bluelovers <codelovers@users.sourceforge.net>
2011-09-23 05:24:12 +0800

    Scorpio_Exception_HTTP

--- Scorpio/libs/Exception.php
+++ Scorpio/libs/Exception.php
@@ -190,7 +190,7 @@ class Scorpio_Exception_Core_ extends Exception {
 			if ( ! headers_sent())
 			{
 				// Make sure the proper http header is sent
-				$http_header_status = ($e instanceof HTTP_Exception) ? $code : 500;
+				$http_header_status = ($e instanceof Scorpio_Exception_HTTP) ? $code : 500;
 
 				header('Content-Type: text/html; charset='.Kohana::$charset, TRUE, $http_header_status);
 			}
