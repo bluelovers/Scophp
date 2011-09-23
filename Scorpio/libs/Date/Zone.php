36c4b3f7ec58ef333a1458e626b1a192b3e2fc9b
bluelovers <codelovers@users.sourceforge.net>
2011-09-24 03:47:38 +0800

    public function getOffsetByZone($timezone)

--- Scorpio/libs/Date/Zone.php
+++ Scorpio/libs/Date/Zone.php
@@ -12,6 +12,26 @@ if (0) {
 
 class Scorpio_Date_Zone_Core_ extends DateTimeZone {
 
+	/**
+	 * @return int
+	 */
+	public function getOffsetByZone($timezone) {
+		if (!isset($timezone)) {
+			$timezone = new DateTimeZone(Scorpio_Date::B_TIMEZONE);
+		} elseif (
+			is_string($timezone)
+			|| !is_a($timezone, 'DateTimeZone')
+		) {
+			$timezone = new DateTimeZone($timezone);
+		}
+
+		$_o = new DateTime('now', $timezone);
+
+		$offset = $this->getOffset($_o);
+
+		return $offset;
+	}
+
 }
 
 ?>
