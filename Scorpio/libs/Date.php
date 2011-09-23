6335ccb48d8df5babb501055d2fb5ced8049048e
bluelovers <codelovers@users.sourceforge.net>
2011-09-23 20:13:15 +0800

    Second amounts for various time increments

--- Scorpio/libs/Date.php
+++ Scorpio/libs/Date.php
@@ -25,6 +25,14 @@ class Scorpio_Date_Core_ extends DateTime {
 
 	const SCO_ISO8601 = 'Y-m-d H:i:s';
 
+	// Second amounts for various time increments
+	const S_YEAR   = 31556926;
+	const S_MONTH  = 2629744;
+	const S_WEEK   = 604800;
+	const S_DAY    = 86400;
+	const S_HOUR   = 3600;
+	const S_MINUTE = 60;
+
 }
 
 ?>
