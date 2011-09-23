1487437c0fd41ead282b9342b72ca2f25d369b63
bluelovers <codelovers@users.sourceforge.net>
2011-09-24 05:11:49 +0800

    const D_TIMEZONE	= 'Asia/Taipei';

--- Scorpio/libs/Date.php
+++ Scorpio/libs/Date.php
@@ -37,6 +37,7 @@ class Scorpio_Date_Core_ extends DateTime {
 	const S_MINUTE		= 60;
 
 	const B_TIMEZONE	= 'GMT';
+	const D_TIMEZONE	= 'Asia/Taipei';
 	static $D_TIMEZONE	= 'Asia/Taipei';
 
 	public function __construct($time = 'now', $timezone = null) {
