827c99f615382fcf5650155d342e86f397e80cea
bluelovers <codelovers@users.sourceforge.net>
2011-09-24 17:33:06 +0800

    public function __construct($timezone)

--- Scorpio/libs/Date/Zone.php
+++ Scorpio/libs/Date/Zone.php
@@ -13,6 +13,17 @@ if (0) {
 class Scorpio_Date_Zone_Core_ extends DateTimeZone {
 
 	/**
+	 * DateTimeZone::__construct -- timezone_open — Creates new DateTimeZone object
+	 *
+	 * @return Scorpio_Date_Zone
+	 */
+	public function __construct($timezone) {
+		parent::__construct($timezone);
+
+		return $this;
+	}
+
+	/**
 	 * i don't know why need $datetime
 	 * didn't see any from diff value by $datetime
 	 *
