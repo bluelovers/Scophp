d5d981516a67a5e5f147ef8834034f55d9c9e52c
bluelovers <codelovers@users.sourceforge.net>
2011-10-10 17:03:50 +0800

    <?php defined('SCORPIO_SYSPATH') or include_once('./_include_header.php') or exit('Install tests must be loaded from within index.php!'); ?>

--- demo/test/install.php
+++ demo/test/install.php
@@ -1,4 +1,4 @@
-<?php defined('SYSPATH') or exit('Install tests must be loaded from within index.php!'); ?>
+<?php defined('SCORPIO_SYSPATH') or include_once('./_include_header.php') or exit('Install tests must be loaded from within index.php!'); ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
 	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 
