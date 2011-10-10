a00c37f067b4095cd2b93d03cce3dc2d42958e21
bluelovers <codelovers@users.sourceforge.net>
2011-10-10 16:16:48 +0800

    class Scorpio_Hook_Exception_Core_ extends Scorpio_Exception

---
+++ Scorpio/libs/Hook/Exception.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * @author bluelovers
+ * @copyright 2010
+ */
+
+if (0) {
+	// for IDE
+	class Scorpio_Hook_Exception extends Scorpio_Hook_Exception_Core_ {}
+}
+
+class Scorpio_Hook_Exception_Core_ extends Scorpio_Exception {
+
+}
+
+?>
