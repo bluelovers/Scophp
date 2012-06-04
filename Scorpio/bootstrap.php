ff367f069cce705a97ae230330907406a81e35ef
bluelovers <codelovers@users.sourceforge.net>
2012-06-04 21:57:38 +0800

    Sco_Loader_Autoloader (Step: 3)
    Step: 3
    
    Commit ChangedLog:
    
    [M ] modified:
    
    	Scorpio/bootstrap.php

--- Scorpio/bootstrap.php
+++ Scorpio/bootstrap.php
@@ -7,9 +7,18 @@
 
 error_reporting(E_ALL ^ E_NOTICE ^ E_DEPRECATED);
 
+//$_SCORPIO_KEY = md5(uniqid('get_included_files', true));
+//$_ENV[$_SCORPIO_KEY]['get_included_files'] = get_included_files();
+
+require_once ('Zend/Loader/Autoloader.php');
+
+Zend_Loader_Autoloader::getInstance()
+	->suppressNotFoundWarnings(true)
+;
+
 if (!defined('SCORPIO_SYSPATH')) {
-	require_once dirname(__FILE__).'/Const/Env.php';
-	require_once dirname(__FILE__).'/Sco/File.php';
+	include_once dirname(__FILE__).'/Const/Env.php';
+	include_once dirname(__FILE__).'/Sco/File.php';
 
 	define('SCORPIO_SYSPATH', Sco_File::dirname(__FILE__, '', 1));
 }
@@ -18,3 +27,9 @@ else
 	require_once SCORPIO_SYSPATH.'Const/Env.php';
 }
 
+Zend_Loader::loadClass('Sco_Loader_Autoloader');
+
+Sco_Loader_Autoloader::getInstance()
+	->pushAutoloader(SCORPIO_SYSPATH, 'Sco_')
+	->setDefaultAutoloader(array('Sco_Loader', 'loadClass'));
+;
