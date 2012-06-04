c03bacb6af325e3d5e7b004943a9bed88e74e621
bluelovers <codelovers@users.sourceforge.net>
2012-06-05 01:30:24 +0800

    . (Step: 3)
    Step: 3
    
    Commit ChangedLog:
    
    [M ] modified:
    
    	Scorpio/bootstrap.php

--- Scorpio/bootstrap.php
+++ Scorpio/bootstrap.php
@@ -7,31 +7,36 @@
 
 $error_reporting = error_reporting(E_ALL ^ E_NOTICE ^ E_DEPRECATED);
 
+define('SCORPIO_MICROTIME', microtime(true));
+define('SCORPIO_TIME', (int)SCORPIO_MICROTIME);
+
+define('REQUEST_TIME', SCORPIO_TIME);
+define('REQUEST_MICROTIME', SCORPIO_MICROTIME);
+
+$_SERVER['REQUEST_TIME'] = REQUEST_TIME;
+$_SERVER['REQUEST_MICROTIME'] = REQUEST_MICROTIME;
+
 //$_SCORPIO_KEY = md5(uniqid('get_included_files', true));
 //$_ENV[$_SCORPIO_KEY]['get_included_files'] = get_included_files();
 
 require_once ('Zend/Loader/Autoloader.php');
 
-Zend_Loader_Autoloader::getInstance()
-	->suppressNotFoundWarnings(true)
-;
+Zend_Loader_Autoloader::getInstance()->suppressNotFoundWarnings(true);
 
-if (!defined('SCORPIO_SYSPATH')) {
-	include_once dirname(__FILE__).'/Const/Env.php';
-	include_once dirname(__FILE__).'/Sco/File.php';
+if (!defined('SCORPIO_SYSPATH'))
+{
+	include_once dirname(__FILE__) . '/Const/Env.php';
+	include_once dirname(__FILE__) . '/Sco/File.php';
 
 	define('SCORPIO_SYSPATH', Sco_File::dirname(__FILE__, '', 1));
 }
 else
 {
-	require_once SCORPIO_SYSPATH.'Const/Env.php';
+	require_once SCORPIO_SYSPATH . 'Const/Env.php';
 }
 
 Zend_Loader::loadClass('Sco_Loader_Autoloader');
 
-Sco_Loader_Autoloader::getInstance()
-	->pushAutoloader(SCORPIO_SYSPATH, 'Sco_', true)
-	->setDefaultAutoloader(array('Sco_Loader', 'loadClass'));
-;
+Sco_Loader_Autoloader::getInstance()->pushAutoloader(SCORPIO_SYSPATH, 'Sco_', true)->setDefaultAutoloader(array('Sco_Loader', 'loadClass'));
 
 error_reporting($error_reporting);
