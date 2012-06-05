b4bc9cd8f83a373d9b6a389a8504303ccb80e9d8
bluelovers <codelovers@users.sourceforge.net>
2012-06-05 17:44:24 +0800

    . (Step: 2)
    Step: 2
    
    Commit ChangedLog:
    
    [M ] modified:
    
    	Scorpio/bootstrap.php

--- Scorpio/bootstrap.php
+++ Scorpio/bootstrap.php
@@ -25,40 +25,43 @@ Zend_Loader_Autoloader::getInstance()->suppressNotFoundWarnings(true);
 
 if (!defined('SCORPIO_PATH_SYS'))
 {
-	include_once dirname(__FILE__) . '/Const/Env.php';
-	include_once dirname(__FILE__) . '/Sco/File/Format.php';
+	include_once (dirname(__FILE__) . '/Const/Env.php');
+	!class_exists('Sco_File_Format') && include (dirname(__FILE__) . '/Sco/File/Format.php');
 
 	define('SCORPIO_PATH_SYS', Sco_File_Format::dirname(__FILE__, '', 1));
 }
 else
 {
-	require_once SCORPIO_PATH_SYS . 'Const/Env.php';
+	Zend_Loader::loadFile('Const/Env.php', SCORPIO_PATH_SYS, true);
 }
 
-$exists = false;
-$get_include_path = get_include_path();
-
-$dir_parent = realpath(SCORPIO_PATH_SYS . '../');
-
-foreach (explode(PATH_SEPARATOR, $get_include_path) as $path)
+if (!class_exists('Sco_File_Format') || !class_exists('Sco_Loader_Autoloader'))
 {
-	if (!$exists && realpath($path) == $dir_parent)
+	$exists = false;
+	$get_include_path = get_include_path();
+
+	$dir_parent = realpath(SCORPIO_PATH_SYS . '../');
+
+	foreach (explode(PATH_SEPARATOR, $get_include_path) as $path)
 	{
-		$exists = true;
-		break;
+		if (!$exists && realpath($path) == $dir_parent)
+		{
+			$exists = true;
+			break;
+		}
 	}
+
+	!$exists && set_include_path($dir_parent . PATH_SEPARATOR . $get_include_path);
+
+	//Zend_Loader::loadClass('Sco_Loader_Autoloader', SCORPIO_PATH_SYS);
+	//Zend_Loader::loadClass('Sco_Loader', SCORPIO_PATH_SYS);
+	!class_exists('Sco_File_Format', false) && Zend_Loader::loadFile('Scorpio/Sco/File/Format.php', null, true);
+	!class_exists('Sco_Loader_Autoloader', false) && Zend_Loader::loadFile('Scorpio/Sco/Loader/Autoloader.php', null, true);
+
+	!$exists && set_include_path($get_include_path);
 }
-
-!$exists && set_include_path($dir_parent . PATH_SEPARATOR . $get_include_path);
-
-//Zend_Loader::loadClass('Sco_Loader_Autoloader', SCORPIO_PATH_SYS);
-//Zend_Loader::loadClass('Sco_Loader', SCORPIO_PATH_SYS);
-Zend_Loader::loadFile('Scorpio/Sco/File/Format.php', null, true);
-Zend_Loader::loadFile('Scorpio/Sco/Loader/Autoloader.php', null, true);
 
 Sco_Loader_Autoloader::getInstance()->pushAutoloader(SCORPIO_PATH_SYS, 'Sco_', true)->setDefaultAutoloader(array('Sco_Loader', 'loadClass'));
-
-!$exists && set_include_path($get_include_path);
 
 error_reporting($error_reporting);
 unset($get_include_path, $path, $exists, $error_reporting, $dir_parent);
