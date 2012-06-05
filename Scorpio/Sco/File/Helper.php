735b7eca9f82e336414402057069df21523e34e5
bluelovers <codelovers@users.sourceforge.net>
2012-06-05 18:36:01 +0800

    Sco_File_Helper
    Commit ChangedLog:
    
    [A ] new file:
    
    	Scorpio/Sco/File/Helper.php

---
+++ Scorpio/Sco/File/Helper.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * @author bluelovers
+ * @copyright 2012
+ */
+
+class Sco_File_Helper
+{
+
+	public static function fp_get_contents($fp)
+	{
+		rewind($fp);
+		return stream_get_contents($fp);
+	}
+
+	public static function fp_put_contents($fp, $data, $flags = 0)
+	{
+		if ($flags & LOCK_EX)
+		{
+			$is_locked = flock($fp, LOCK_EX);
+		}
+
+		fseek($fp, 0, SEEK_SET);
+		ftruncate($fp, 0);
+		rewind($fp);
+		$return = fputs($fp, $data);
+
+		if ($is_locked) flock($fp, LOCK_UN);
+
+		return $return;
+	}
+
+}
