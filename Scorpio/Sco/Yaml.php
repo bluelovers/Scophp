72ed80f69fe6b5b8a2b0ea00aa3b6af6cbfc9b3d
bluelovers <codelovers@users.sourceforge.net>
2012-06-05 23:19:47 +0800

    . (Step: 4)
    Step: 4
    
    Commit ChangedLog:
    
    [M ] modified:
    
    	Scorpio/Sco/Yaml.php

--- Scorpio/Sco/Yaml.php
+++ Scorpio/Sco/Yaml.php
@@ -10,6 +10,9 @@ class Sco_Yaml extends Symfony_Component_Yaml_Yaml
 
 	const INLINE = 6;
 
+	protected static $_enableDefaultDumpFilter = false;
+	protected static $_defaultDumpFilter;
+
 	public static function load($file, $enablePhpParsing = false)
 	{
 		if (Sco_File_Helper::is_resource_file($file))
@@ -30,9 +33,9 @@ class Sco_Yaml extends Symfony_Component_Yaml_Yaml
 		return $yaml;
 	}
 
-	public static function save($file, $data, $inline = self::INLINE)
+	public static function save($file, $data, $inline = self::INLINE, $callback = null)
 	{
-		$dump = self::dump($data, $inline);
+		$dump = self::dump($data, $inline, $callback);
 
 		if (Sco_File_Helper::is_resource_file($file))
 		{
@@ -46,13 +49,44 @@ class Sco_Yaml extends Symfony_Component_Yaml_Yaml
 		return $ret;
 	}
 
-	public static function dump($array, $inline = self::INLINE)
+	public static function dump($array, $inline = self::INLINE, $callback = null)
 	{
 		if (is_object($array) && method_exists($array, 'toYaml')) {
-            return $array->toYaml();
+            return $array->toYaml($inline, $callback);
         }
 
-		return parent::dump($array, $inline);
+        if ($callback && is_callable($callback))
+        {
+        	$dump = call_user_func($callback, $array, $inline);
+        }
+        elseif (self::$_enableDefaultDumpFilter && is_callable(self::$_defaultDumpFilter))
+        {
+        	$dump = call_user_func(self::$_defaultDumpFilter, $array, $inline);
+        }
+        else
+        {
+        	$dump = $array;
+        }
+
+		return parent::dump($dump, $inline);
+	}
+
+	public static function setDefaultDumpFilter($callback)
+	{
+		self::$_defaultDumpFilter = $callback;
+	}
+
+	public static function enableDefaultDumpFilter($flag = null) {
+		if (null === $flag)
+		{
+			return self::$_enableDefaultDumpFilter;
+		}
+
+		$old = self::$_enableDefaultDumpFilter;
+
+		self::$_enableDefaultDumpFilter = (bool)$flag;
+
+		return $old;
 	}
 
 }
