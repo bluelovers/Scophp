77773813c2d4d589e41f004da761fe24be8f304f
bluelovers <codelovers@users.sourceforge.net>
2012-06-05 11:51:11 +0800

    .
    Commit ChangedLog:
    
    [M ] modified:
    
    	Scorpio/bootstrap.php

--- Scorpio/bootstrap.php
+++ Scorpio/bootstrap.php
@@ -23,22 +23,43 @@ require_once ('Zend/Loader/Autoloader.php');
 
 Zend_Loader_Autoloader::getInstance()->suppressNotFoundWarnings(true);
 
-if (!defined('SCORPIO_SYSPATH'))
+if (!defined('SCORPIO_PATH_SYS'))
 {
 	include_once dirname(__FILE__) . '/Const/Env.php';
 	include_once dirname(__FILE__) . '/Sco/File.php';
 
-	define('SCORPIO_SYSPATH', Sco_File::dirname(__FILE__, '', 1));
+	define('SCORPIO_PATH_SYS', Sco_File::dirname(__FILE__, '', 1));
+	define('SCORPIO_PATH_TOP', Sco_File::dirname(__FILE__, '..', 1));
 }
 else
 {
-	require_once SCORPIO_SYSPATH . 'Const/Env.php';
+	require_once SCORPIO_PATH_SYS . 'Const/Env.php';
 }
 
-set_include_path(SCORPIO_SYSPATH . PATH_SEPARATOR . get_include_path());
+$exists = false;
+$paths = array();
+foreach (explode(PATH_SEPARATOR, get_include_path()) as $path)
+{
+	$path = Sco_File::path($path);
 
-Zend_Loader::loadClass('Sco_Loader_Autoloader');
+	if (!$exists && $path == SCORPIO_PATH_TOP)
+	{
+		$exists = true;
+	}
 
-Sco_Loader_Autoloader::getInstance()->pushAutoloader(SCORPIO_SYSPATH, 'Sco_', true)->setDefaultAutoloader(array('Sco_Loader', 'loadClass'));
+	$paths[] = $path;
+}
+
+if (!$exists) array_unshift($paths, SCORPIO_PATH_TOP);
+
+set_include_path(implode(PATH_SEPARATOR, $paths));
+unset($paths, $path, $exists);
+
+//Zend_Loader::loadClass('Sco_Loader_Autoloader', SCORPIO_PATH_SYS);
+//Zend_Loader::loadClass('Sco_Loader', SCORPIO_PATH_SYS);
+//Zend_Loader::loadFile('Scorpio/Sco/Loader.php');
+Zend_Loader::loadFile('Scorpio/Sco/Loader/Autoloader.php');
+
+Sco_Loader_Autoloader::getInstance()->pushAutoloader(SCORPIO_PATH_SYS, 'Sco_', true)->setDefaultAutoloader(array('Sco_Loader', 'loadClass'));
 
 error_reporting($error_reporting);
