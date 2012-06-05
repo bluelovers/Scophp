147073b737264234400ab640936a1c86bf18335a
bluelovers <codelovers@users.sourceforge.net>
2012-06-05 22:08:12 +0800

    Yaml (Step: 2)
    Step: 2
    
    Commit ChangedLog:
    
    [A ] new file:
    
    	Test/bootstrap.php

---
+++ Test/bootstrap.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * @author bluelovers
+ * @copyright 2012
+ */
+
+require_once (dirname(__FILE__).'/../Scorpio/bootstrap.php');
+
+Sco_Loader_Autoloader::getInstance()->pushAutoloader('D:\Users\Documents\The Project\symfony\symfony1\Yaml', 'Symfony_Component_Yaml_');
+
