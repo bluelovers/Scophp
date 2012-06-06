3d44713995eb48c0050447195f24f79036f164b8
bluelovers <codelovers@users.sourceforge.net>
2012-06-06 11:30:59 +0800

    Sco_Array_Dumper_Helper (Step: 1)
    Step: 1
    
    Commit ChangedLog:
    
    [M ] modified:
    
    	Scorpio/Sco/Array/Dumper/Helper.php

--- Scorpio/Sco/Array/Dumper/Helper.php
+++ Scorpio/Sco/Array/Dumper/Helper.php
@@ -8,7 +8,40 @@
 class Sco_Array_Dumper_Helper
 {
 
+	public static function toArrayRecursive($obj, $filter_empty = true)
+	{
+		if ($filter_empty)
+		{
+			$dump = null;
+		}
+		else
+		{
+			$dump = array();
+		}
 
+		foreach ($obj as $k => $v)
+		{
+			if (($filter_empty && is_array($v) && empty($v)) || $v === null)
+			{
+				continue;
+			}
+			elseif (is_array($v) || $v instanceof Traversable)
+			{
+				$_v = self::toArrayRecursive($v, $filter_empty);
+
+				if (!(($filter_empty && is_array($_v) && empty($_v)) || $_v === null))
+				{
+					$dump[$k] = $_v;
+				}
+			}
+			else
+			{
+				$dump[$k] = $v;
+			}
+		}
+
+		return $dump;
+	}
 
 }
 
