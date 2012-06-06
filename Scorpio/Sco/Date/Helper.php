3fecb9e9ddb0f6256cbd37f724ae49956f6140fb
bluelovers <codelovers@users.sourceforge.net>
2012-06-06 15:02:36 +0800

    Helper (Step: 1)
    Step: 1
    
    Commit ChangedLog:
    
    [A ] new file:
    
    	Scorpio/Sco/Date/Helper.php

---
+++ Scorpio/Sco/Date/Helper.php
@@ -0,0 +1,48 @@
+<?php
+
+/**
+ * @author bluelovers
+ * @copyright 2012
+ */
+
+/**
+ * $t[] = Sco_Date_Helper::microtime();
+ * $t[] = Sco_Date_Helper::microtime_split($t[0][1].$t[0][0]);
+ * var_dump($t);
+ */
+class Sco_Date_Helper
+{
+
+	const MICROTIME_LEN = 8;
+
+	function microtime($get_as_float = false)
+	{
+		$microtime = microtime(true);
+
+		$time = floor($microtime);
+
+		// 0.31520000
+		//$microsec = bcsub((float)$microtime, (float)$time, 8);
+
+		// 0.15699505805969
+		$microsec = $microtime - $time;
+
+		if ($get_as_float)
+		{
+			return (float)$time + (float)$microsec;
+		}
+		else
+		{
+			return array((int)substr($microsec, 2, self::MICROTIME_LEN), (int)$time, $microtime);
+		}
+	}
+
+	function microtime_split($microtime)
+	{
+		$time = substr($microtime, 0, 10);
+		$microsec = substr($microtime, 10);
+
+		return array((int)$microsec, (int)$time);
+	}
+
+}
