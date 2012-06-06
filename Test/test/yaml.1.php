5ff93445358db3da02ff5cb976cab51eaf835f9d
bluelovers <codelovers@users.sourceforge.net>
2012-06-06 11:31:01 +0800

    Sco_Array_Dumper_Helper (Step: 3)
    Step: 3
    
    Commit ChangedLog:
    
    [M ] modified:
    
    	Test/test/yaml.1.php

--- Test/test/yaml.1.php
+++ Test/test/yaml.1.php
@@ -9,6 +9,31 @@ require_once ('../bootstrap.php');
 
 $arr = new Sco_Array();
 
-$dump = Sco_Yaml::dump($arr);
 
-var_dump($dump);
+$arr->k = 1;
+
+$arr[] = array(
+	array(
+		new Sco_Array(),
+	),
+
+	clone $arr,
+
+	'd' => array(),
+);
+
+$arr->kk = array(
+	123,
+	new Sco_Array(),
+	clone $arr,
+);
+
+$arr[] = array();
+
+$dump = Sco_Array_Dumper_Helper::toArrayRecursive($arr);
+
+var_dump($dump, Sco_Yaml::dump($dump));
+
+$dump = Sco_Array_Dumper_Helper::toArrayRecursive($arr, false);
+
+var_dump($dump, Sco_Yaml::dump($dump));
