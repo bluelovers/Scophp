2d98cd4df6a69f3202c8a125141b6e3eedf643a7
bluelovers <codelovers@users.sourceforge.net>
2012-06-07 17:48:39 +0800

    . (Step: 2)
    Step: 2
    
    Commit ChangedLog:
    
    [M ] modified:
    
    	Test/test/text.printf.2.php

--- Test/test/text.printf.2.php
+++ Test/test/text.printf.2.php
@@ -8,10 +8,11 @@
 require_once ('../bootstrap.php');
 
 Sco_Text_Format::suppressArgvWarnings(true);
+Sco_Text_Format::forceMode(true);
 
 $test_list = array();
 
-$n =  array('n' => 43951789);
+$n = array('n' => 43951789);
 $u = array('u' => -43951789);
 $c = array('c' => 65); // ASCII 65 is 'A'
 
@@ -34,17 +35,20 @@ array_push(&$test_list, array("%+d", $u)); // sign specifier on a negative integ
 array_push(&$test_list, array("%0+20d", $n));
 array_push(&$test_list, array("%0+20d", $u));
 
+array_push(&$test_list, array("%+020d", $n));
+array_push(&$test_list, array("%+020d", $u));
+
 array_push(&$test_list, array("%'.+20d", $n));
 array_push(&$test_list, array("%'.+20d", $u));
 
 $s = 'monkey';
 $t = 'many monkeys';
 
-array_push(&$test_list, array("[%s]",      $s)); // standard string output
-array_push(&$test_list, array("[%10s]",    $s)); // right-justification with spaces
-array_push(&$test_list, array("[%-10s]",   $s)); // left-justification with spaces
-array_push(&$test_list, array("[%010s]",   $s)); // zero-padding works on strings too
-array_push(&$test_list, array("[%'#10s]",  $s)); // use the custom padding character '#'
+array_push(&$test_list, array("[%s]", $s)); // standard string output
+array_push(&$test_list, array("[%10s]", $s)); // right-justification with spaces
+array_push(&$test_list, array("[%-10s]", $s)); // left-justification with spaces
+array_push(&$test_list, array("[%010s]", $s)); // zero-padding works on strings too
+array_push(&$test_list, array("[%'#10s]", $s)); // use the custom padding character '#'
 array_push(&$test_list, array("[%10.10s]", $t)); // left-justification but with a cutoff of 10 characters
 
 array_push(&$test_list, array("second: %2\$s ; first: %1\$s", array('1st', '2nd')));
@@ -53,32 +57,67 @@ array_push(&$test_list, array('second: %second$s ; first: %first$s', array('1st'
 
 array_push(&$test_list, array("second: %2\$s ; first: %1\$s ; second: %2\$s ; first: %1\$s; ", array('1st', '2nd')));
 
-array_push(&$test_list, array("second: %2\$s ; %s ; first: %1\$s ; %s ; second: %2\$s ; %s ; first: %1\$s ; %s ; %(v5)s", array('1st', '2nd', '3nd')));
+array_push(&$test_list, array("second: %2\$s ; %s ; first: %1\$s ; %s ; second: %2\$s ; %s ; first: %1\$s ; %s ; %(v5)s", array(
+		'1st',
+		'2nd',
+		'3nd')));
+
+array_push(&$test_list, array("%04d-%02d-%02d", explode('-', '1988-8-1')));
+
+array_push(&$test_list, array("Hello, %(place)s, how is it hanning at %(place)s? %s works just as well", array(
+		'place' => 'world',
+		'sprintf',
+		'not used')));
+
+array_push(&$test_list, array("%3s:%15s", array("Mo", "09:30-19:00")));
+array_push(&$test_list, array("%3s:%15s", array("Di", "09:30-19:00")));
+array_push(&$test_list, array("%3s:%15s", array("So", "geschlossen")));
+
+array_push(&$test_list, array("%("));
+array_push(&$test_list, array("%)"));
+array_push(&$test_list, array("%%"));
+array_push(&$test_list, array("%$"));
+array_push(&$test_list, array("%/"));
+array_push(&$test_list, array("%\\"));
+array_push(&$test_list, array("%0"));
+array_push(&$test_list, array("%|"));
+array_push(&$test_list, array("%%("));
+array_push(&$test_list, array("%%)"));
+array_push(&$test_list, array("%%%"));
+array_push(&$test_list, array("%%$"));
+array_push(&$test_list, array("%%/"));
+array_push(&$test_list, array("%%\\"));
+array_push(&$test_list, array("%%0"));
+array_push(&$test_list, array("%%|"));
+array_push(&$test_list, array("%(1\$s%)", array('123', array(456))));
+array_push(&$test_list, array("%(s%)", array('123', array(456))));
 
 foreach ($test_list as $data)
 {
-	list ($format, $args) = $data;
+	list($format, $args) = $data;
 
-	echo str_repeat('=', 80).LF;
-	echo $format.LF;
-	echo str_repeat('-', 80).LF;
+	echo str_repeat('=', 80) . LF;
+	echo $format . LF;
+	echo str_repeat('-', 80) . LF;
+
+	$args = (array )$args;
 
 	$time = microtime(true);
 
-	$orig = vsprintf($format, (array)$args);
+	$orig = vsprintf($format, $args);
 
 	$time1 = microtime(true);
 
-	$frame = Sco_Text_Format::vsprintf($format, (array)$args);
+	$frame = Sco_Text_Format::vsprintf($format, $args);
 
 	$time2 = microtime(true);
 
 	$error = ($frame !== $orig);
 
-	echo $orig.LF;
+	var_dump($orig);
 	printf('Processed in %.8f second(s)' . LF, $time1 - $time);
 	printf('<span style="color: %s">', $error ? 'red' : '#cccccc');
-	echo $frame.LF;
-	echo('</span>');
+	var_dump($frame);
+	echo ('</span>');
 	printf('Processed in %.8f second(s)' . LF, $time2 - $time1);
 }
