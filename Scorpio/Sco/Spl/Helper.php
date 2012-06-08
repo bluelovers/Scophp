ba368e2359ed683affd66fa0c78dec07d40f2a63
bluelovers <codelovers@users.sourceforge.net>
2012-06-09 03:28:57 +0800

    . (Step: 12)
    Step: 12
    
    Commit ChangedLog:
    
    [M ] modified:
    
    	Scorpio/Sco/Spl/Helper.php

--- Scorpio/Sco/Spl/Helper.php
+++ Scorpio/Sco/Spl/Helper.php
@@ -12,9 +12,19 @@ class Sco_Spl_Helper
 
 	public static function createFunction($func_name, $callback)
 	{
+		if (is_array($func_name))
+		{
+			foreach ($func_name as $func)
+			{
+				self::createFunction($func, $callback);
+			}
+
+			return $func_name;
+		}
+
 		if (!self::vaildFunctionName($func_name))
 		{
-			throw new RuntimeException(sprintf('Fatal error: syntax error "%s" not a vaild Function name', (string)$func_name));
+			throw new RuntimeException(sprintf('Fatal error: syntax error "%s" not a vaild Function name', (string )$func_name));
 			//trigger_error(sprintf('syntax error "%s" not a vaild Function name', (string)$func_name), E_USER_ERROR);
 			return;
 		}
@@ -27,13 +37,13 @@ class Sco_Spl_Helper
 		{
 			if (function_exists($func_name))
 			{
-				throw new RuntimeException(sprintf('Fatal error: Cannot redeclare Function %s() previously declared', (string)$func_name));
+				throw new RuntimeException(sprintf('Fatal error: Cannot redeclare Function %s() previously declared', (string )$func_name));
 				return;
 			}
 
 			$eval = 'function %s(){ return call_user_func_array(Sco_Spl_Helper::$lambda[\'%s\'], func_get_args()); }';
 
-			eval(sprintf($eval, (string)$func_name, (string)$func_name));
+			eval(sprintf($eval, (string )$func_name, (string )$func_name));
 		}
 
 		return $func_name;
