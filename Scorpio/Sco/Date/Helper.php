55ff41dc04d0af09022da910e7a268d777a538ed
bluelovers <codelovers@users.sourceforge.net>
2012-06-09 06:00:47 +0800

    . (Step: 2)
    Step: 2
    
    Commit ChangedLog:
    
    [M ] modified:
    
    	Scorpio/Sco/Date/Helper.php

--- Scorpio/Sco/Date/Helper.php
+++ Scorpio/Sco/Date/Helper.php
@@ -15,34 +15,81 @@ class Sco_Date_Helper
 
 	const MICROTIME_LEN = 8;
 
-	function microtime($get_as_float = false)
+	public static function microtime($get_as_float = false, $microtime = null)
 	{
-		$microtime = microtime(true);
-
-		$time = floor($microtime);
-
-		// 0.31520000
-		//$microsec = bcsub((float)$microtime, (float)$time, 8);
-
-		// 0.15699505805969
-		$microsec = $microtime - $time;
-
-		if ($get_as_float)
+		if ($microtime)
 		{
-			return (float)$time + (float)$microsec;
+			if (is_array($microtime))
+			{
+				list($microsec, $time) = $microtime;
+
+				return array(
+					sprintf('%-08.8s', (string )$microsec),
+					(int)$time,
+					(float)((float)$time + (float)('0.' . (string )$microsec)),
+					);
+			}
 		}
 		else
 		{
-			return array((int)substr($microsec, 2, self::MICROTIME_LEN), (int)$time, $microtime);
+			$microtime = microtime(true);
+		}
+
+		if ($get_as_float)
+		{
+			return (float)$microtime;
+		}
+		else
+		{
+			$time = floor($microtime);
+
+			// 0.31520000
+			//$microsec = bcsub((float)$microtime, (float)$time, 8);
+
+			// 0.15699505805969
+			$microsec = (float)$microtime - $time;
+
+			return array(
+				(string )substr($microsec, 2, self::MICROTIME_LEN),
+				(int)$time,
+				(float)$microtime,
+				);
 		}
 	}
 
-	function microtime_split($microtime)
+	public static function microtime_split($microtime)
 	{
 		$time = substr($microtime, 0, 10);
-		$microsec = substr($microtime, 10);
+		$microsec = substr($microtime, 10, self::MICROTIME_LEN);
 
-		return array((int)$microsec, (int)$time);
+		return array((string )$microsec, (int)$time);
+	}
+
+	public static function date_format_fix($format, $timestamp)
+	{
+		$microtime = $u = null;
+
+		$pos = 0;
+		while (preg_match('`(?<!\\\\)u`', $format, $match, PREG_OFFSET_CAPTURE, $pos))
+		{
+			if (!isset($u))
+			{
+				if (!isset($microtime))
+				{
+					$microtime = self::microtime(false, $timestamp);
+				}
+
+				$u = sprintf('%-06.6s', $microtime[0]);
+			}
+
+			$format = substr_replace($format, $u, $match[0][1], 1);
+
+			$pos = $match[0][1] + 6;
+		}
+
+		//var_dump($timestamp, $microtime, self::microtime_split($microtime[1].$microtime[0]));
+
+		return $format;
 	}
 
 }
