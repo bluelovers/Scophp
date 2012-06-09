05ce86427b40bd45e069e4f5a2f4bebf45ace386
bluelovers <codelovers@users.sourceforge.net>
2012-06-10 05:51:56 +0800

    Sco/PHP/Handler/Error (Step: 7)
    Step: 7
    
    Commit ChangedLog:
    
    [A ] new file:
    
    	Test/test/error.1.php

---
+++ Test/test/error.1.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * @author bluelovers
+ * @copyright 2012
+ */
+
+require_once ('../bootstrap.php');
+
+var_dump(Sco_PHP_Handler_Error::last_result(), Sco_PHP_Handler_Error::get(), Sco_PHP_Handler_Error::state(), Sco_PHP_Handler_Error::has_started(), Sco_PHP_Handler_Error::restore(), Sco_PHP_Handler_Error::get(), Sco_PHP_Handler_Error::get(true));
