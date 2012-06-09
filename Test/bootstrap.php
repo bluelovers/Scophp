0df77b01c9c3f6d8455d12ba04c61c20038025b7
bluelovers <codelovers@users.sourceforge.net>
2012-06-10 05:51:59 +0800

    Sco/PHP/Handler/Error (Step: 11)
    Step: 11
    
    Commit ChangedLog:
    
    [M ] modified:
    
    	Test/bootstrap.php

--- Test/bootstrap.php
+++ Test/bootstrap.php
@@ -16,8 +16,6 @@ echo '<meta http-equiv="Content-Type" content="text/html; charset=utf-8" /><pre>
 
 Sco_Loader_Autoloader::getInstance()->pushAutoloader('D:\Users\Documents\The Project\symfony\symfony1\Yaml', 'Symfony_Component_Yaml_');
 
-register_shutdown_function('_shutdown_function');
-
 function _shutdown_function()
 {
 	$unit = array(
@@ -36,12 +34,50 @@ function _shutdown_function()
 
 	$get_included_files = get_included_files();
 
-	echo NL.NL;
+	echo NL . NL;
 	//printf('Processed in %.8f second(s), %d io, %s/%s.', microtime(true) - SCORPIO_MICROTIME, count(get_included_files()), $size1, $size2);
 
 	$table = new Zend_Text_Table(array('columnWidths' => array(80)));
-	$table->appendRow(array(sprintf('Processed in %.8f second(s), %d io, %s/%s.', microtime(true) - SCORPIO_MICROTIME, count($get_included_files), $size1, $size2)));
+	$table->appendRow(array(sprintf('Processed in %.8f second(s), %d io, %s/%s. PHP %s %s', microtime(true) - SCORPIO_MICROTIME, count($get_included_files), $size1, $size2, PHP_VERSION, PHP_OS)));
 	echo $table;
 }
 
+function _error_handler($code, $msg, $file, $line)
+{
+	if (0 && !(error_reporting() & $code))
+	{
+		return;
+	}
+
+	$typestr = Sco_PHP::errno_const($code);
+
+	$file = Sco_File_Format::remove_root($file, SCORPIO_PATH_SYS . '../../');
+
+	printf('<div>%s: %s in %s %d</div>', $typestr, $msg, $file, $line);
+
+	return true;
+}
+
+function _fatal_error_handler()
+{
+	if ($e = @error_get_last())
+	{
+		if ($e['type'] & E_FATAL)
+		{
+			error_handler($e['type'], $e['message'], $e['file'], $e['line']);
+		}
+	}
+
+	shutdown_function();
+
+	exit;
+}
+
 Sco_Spl_Helper::createFunction('printnl', 'Sco_PHP_Helper::sprintnl');
+Sco_Spl_Helper::createFunction('shutdown_function', '_shutdown_function');
+Sco_Spl_Helper::createFunction('error_handler', '_error_handler');
+Sco_Spl_Helper::createFunction('fatal_error_handler', '_fatal_error_handler');
+
+$old = Sco_PHP_Handler_Error::start('error_handler');
+
+register_shutdown_function('fatal_error_handler');
