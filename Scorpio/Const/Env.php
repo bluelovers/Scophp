bdf785ecab91f8e13d05fffea855d669bf7cbd78
bluelovers <codelovers@users.sourceforge.net>
2012-06-10 05:51:57 +0800

    Sco/PHP/Handler/Error (Step: 8)
    Step: 8
    
    Commit ChangedLog:
    
    [M ] modified:
    
    	Scorpio/Const/Env.php

--- Scorpio/Const/Env.php
+++ Scorpio/Const/Env.php
@@ -9,10 +9,16 @@ define('DIR_SEP', '/');
 
 define('LF', "\n");
 define('CR', "\r");
-define('CRLF', CR.LF);
+define('CRLF', CR . LF);
 
 define('TAB', "\t");
 
 !defined('E_DEPRECATED') and define('E_DEPRECATED', 8192);
+
+/**
+ * @link http://stackoverflow.com/questions/277224/how-do-i-catch-a-php-fatal-error
+ */
+define('E_FATAL', E_ERROR | E_USER_ERROR | E_PARSE | E_CORE_ERROR | E_COMPILE_ERROR | E_RECOVERABLE_ERROR);
+define('ERROR_REPORTING', E_ALL | E_STRICT);
 
 ?>
