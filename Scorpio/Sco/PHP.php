ce19d1df8f1b2e2d49910f7c3b63d0c8a055c16d
bluelovers <codelovers@users.sourceforge.net>
2012-06-11 04:48:24 +0800

    final class Sco (Step: 4)
    Step: 4
    
    Commit ChangedLog:
    
    [M ] modified:
    
    	Scorpio/Sco/PHP.php

--- Scorpio/Sco/PHP.php
+++ Scorpio/Sco/PHP.php
@@ -18,89 +18,6 @@ class Sco_PHP
 	 */
 	protected static $_shutdown_registed;
 
-	public static function void()
-	{
-		return null;
-	}
-
-	public static function errno_const($errno, $chk = false)
-	{
-
-		switch ($errno)
-		{
-			case E_ERROR:
-				// 1
-				$typestr = 'E_ERROR';
-				break;
-			case E_WARNING:
-				// 2
-				$typestr = 'E_WARNING';
-				break;
-			case E_PARSE:
-				// 4
-				$typestr = 'E_PARSE';
-				break;
-			case E_NOTICE:
-				// 8
-				$typestr = 'E_NOTICE';
-				break;
-			case E_CORE_ERROR:
-				// 16
-				$typestr = 'E_CORE_ERROR';
-				break;
-			case E_CORE_WARNING:
-				// 32
-				$typestr = 'E_CORE_WARNING';
-				break;
-			case E_COMPILE_ERROR:
-				// 64
-				$typestr = 'E_COMPILE_ERROR';
-				break;
-			case E_CORE_WARNING:
-				// 128
-				$typestr = 'E_COMPILE_WARNING';
-				break;
-			case E_USER_ERROR:
-				// 256
-				$typestr = 'E_USER_ERROR';
-				break;
-			case E_USER_WARNING:
-				// 512
-				$typestr = 'E_USER_WARNING';
-				break;
-			case E_USER_NOTICE:
-				// 1024
-				$typestr = 'E_USER_NOTICE';
-				break;
-			case E_STRICT:
-				// 2048
-				$typestr = 'E_STRICT';
-				break;
-			case E_RECOVERABLE_ERROR:
-				// 4096
-				$typestr = 'E_RECOVERABLE_ERROR';
-				break;
-			case E_DEPRECATED:
-				// 8192
-				$typestr = 'E_DEPRECATED';
-				break;
-
-			case E_USER_DEPRECATED:
-				// 16384
-				$typestr = 'E_USER_DEPRECATED';
-				break;
-			default:
-				$fail = true;
-				$typestr = 'E_UNKNOW[' . $errno . ']';
-				break;
-		}
-
-		return $chk ? array(
-			$fail,
-			$typestr,
-			$errno) : $typestr;
-	}
-
 	/**
 	 * @return Sco_Spl_Callback_Iterator
 	 */
@@ -153,6 +70,39 @@ class Sco_PHP
 		}
 
 		return true;
+	}
+
+	public static function error_reporting($level = null, $add = null)
+	{
+		if ($level === null)
+		{
+			return error_reporting();
+		}
+		else
+		{
+
+			if (defined('E_DEPRECATED'))
+			{
+				$level = $level ^ E_DEPRECATED;
+			}
+
+			if ($add !== null)
+			{
+				$level |= $add;
+			}
+
+			return error_reporting($level);
+		}
+	}
+
+	public static function prepend_include_path($path)
+	{
+		return set_include_path($path . PATH_SEPARATOR . get_include_path());
+	}
+
+	public static function append_include_path($path)
+	{
+		return set_include_path(get_include_path() . PATH_SEPARATOR . $path);
 	}
 
 }
