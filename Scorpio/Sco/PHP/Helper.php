649dfb480f2e9fa7dd15e48a1fc5b92967d98ec4
bluelovers <codelovers@users.sourceforge.net>
2012-06-11 04:48:26 +0800

    final class Sco (Step: 6)
    Step: 6
    
    Commit ChangedLog:
    
    [M ] modified:
    
    	Scorpio/Sco/PHP/Helper.php

--- Scorpio/Sco/PHP/Helper.php
+++ Scorpio/Sco/PHP/Helper.php
@@ -8,11 +8,6 @@
 class Sco_PHP_Helper
 {
 
-	public static function prepend_include_path($path)
-	{
-		return set_include_path($path . PATH_SEPARATOR . get_include_path());
-	}
-
 	public static function var_ini_bool($val)
 	{
 		if (is_bool($val) === true)
@@ -126,29 +121,6 @@ class Sco_PHP_Helper
 		return $varList;
 	}
 
-	public static function error_reporting($level = null, $add = null)
-	{
-		if ($level === null)
-		{
-			return error_reporting();
-		}
-		else
-		{
-
-			if (defined('E_DEPRECATED'))
-			{
-				$level = $level ^ E_DEPRECATED;
-			}
-
-			if ($add !== null)
-			{
-				$level |= $add;
-			}
-
-			return error_reporting($level);
-		}
-	}
-
 	/**
 	 * Checks if the class method exists in the given object .
 	 *
@@ -189,6 +161,89 @@ class Sco_PHP_Helper
 	public static function vprintnl($args)
 	{
 		return self::vprint($args, '', NL);
+	}
+
+	public static function void()
+	{
+		return null;
+	}
+
+	public static function errno_const($errno, $chk = false)
+	{
+
+		switch ($errno)
+		{
+			case E_ERROR:
+				// 1
+				$typestr = 'E_ERROR';
+				break;
+			case E_WARNING:
+				// 2
+				$typestr = 'E_WARNING';
+				break;
+			case E_PARSE:
+				// 4
+				$typestr = 'E_PARSE';
+				break;
+			case E_NOTICE:
+				// 8
+				$typestr = 'E_NOTICE';
+				break;
+			case E_CORE_ERROR:
+				// 16
+				$typestr = 'E_CORE_ERROR';
+				break;
+			case E_CORE_WARNING:
+				// 32
+				$typestr = 'E_CORE_WARNING';
+				break;
+			case E_COMPILE_ERROR:
+				// 64
+				$typestr = 'E_COMPILE_ERROR';
+				break;
+			case E_CORE_WARNING:
+				// 128
+				$typestr = 'E_COMPILE_WARNING';
+				break;
+			case E_USER_ERROR:
+				// 256
+				$typestr = 'E_USER_ERROR';
+				break;
+			case E_USER_WARNING:
+				// 512
+				$typestr = 'E_USER_WARNING';
+				break;
+			case E_USER_NOTICE:
+				// 1024
+				$typestr = 'E_USER_NOTICE';
+				break;
+			case E_STRICT:
+				// 2048
+				$typestr = 'E_STRICT';
+				break;
+			case E_RECOVERABLE_ERROR:
+				// 4096
+				$typestr = 'E_RECOVERABLE_ERROR';
+				break;
+			case E_DEPRECATED:
+				// 8192
+				$typestr = 'E_DEPRECATED';
+				break;
+
+			case E_USER_DEPRECATED:
+				// 16384
+				$typestr = 'E_USER_DEPRECATED';
+				break;
+			default:
+				$fail = true;
+				$typestr = 'E_UNKNOW[' . $errno . ']';
+				break;
+		}
+
+		return $chk ? array(
+			$fail,
+			$typestr,
+			$errno) : $typestr;
 	}
 
 }
