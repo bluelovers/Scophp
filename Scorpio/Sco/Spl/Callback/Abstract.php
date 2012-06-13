435f5030847286a153beba2ac796bd09999da0d6
bluelovers <codelovers@users.sourceforge.net>
2012-06-14 01:34:10 +0800

    . (Step: 3)
    Step: 3
    
    Commit ChangedLog:
    
    [M ] modified:
    
    	Scorpio/Sco/Spl/Callback/Abstract.php

--- Scorpio/Sco/Spl/Callback/Abstract.php
+++ Scorpio/Sco/Spl/Callback/Abstract.php
@@ -30,4 +30,14 @@ abstract class Sco_Spl_Callback_Abstract implements Sco_Spl_Callback_Interface
 		return Sco_Spl_Helper::create_function($func_name, $this->callback());
 	}
 
+	/**
+	 * PHP version >= 5.3
+	 */
+	public function __invoke()
+	{
+		$argv = func_num_args() > 0 ? func_get_args() : (array)$this->argv;
+
+		return $this->exec_array($argv);
+	}
+
 }
