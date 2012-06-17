49a695e6623b2b658b2e448e08a3b8231072c6f5
bluelovers <codelovers@users.sourceforge.net>
2012-06-17 16:51:30 +0800

    Sco_Filter_IdentityCard_ROC (Step: 4)
    Step: 4
    
    Commit ChangedLog:
    
    [M ] modified:
    
    	Test/test/error.1.php

--- Test/test/error.1.php
+++ Test/test/error.1.php
@@ -8,3 +8,5 @@
 require_once ('../bootstrap.php');
 
 var_dump(Sco_PHP_Handler_Error::last_result(), Sco_PHP_Handler_Error::get(), Sco_PHP_Handler_Error::state(), Sco_PHP_Handler_Error::has_started(), Sco_PHP_Handler_Error::restore(), Sco_PHP_Handler_Error::get(), Sco_PHP_Handler_Error::get(true));
+
+var_dump(E_ERROR & E_FATAL);
