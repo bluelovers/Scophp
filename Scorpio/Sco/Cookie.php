5c7faf5b3c6a43d6361886395ea2a2b61f391531
bluelovers <codelovers@users.sourceforge.net>
2012-06-20 06:13:16 +0800

    Sco_Cookie (Step: 1)
    Step: 1
    
    Commit ChangedLog:
    
    [R ] renamed:
    
    	Scorpio/Sco/Cookie/Object.php -> Scorpio/Sco/Cookie.php

--- Scorpio/Sco/Cookie.php
+++ Scorpio/Sco/Cookie.php
@@ -5,8 +5,10 @@
  * @copyright 2012
  */
 
-class Sco_Cookie_Object extends ArrayObject
+class Sco_Cookie
 {
+
+	const EXPIRE_NOW = -1;
 
 	/**
 	 * Class name of the singleton registry object.
@@ -19,22 +21,6 @@ class Sco_Cookie_Object extends ArrayObject
 	 * @var Sco_Cookie_Object
 	 */
 	private static $_registry = null;
-
-	protected $_config = array(
-
-		'default_expire' => 0,
-		'default_path' => '/',
-		'default_domain' => '.',
-		'default_secure' => false,
-		'default_httponly' => false,
-
-		'autosave' => true,
-
-		);
-
-	protected $_cookies;
-
-	const EXPIRE_NOW = -1;
 
 	/**
 	 * Retrieves the default registry instance.
@@ -73,19 +59,19 @@ class Sco_Cookie_Object extends ArrayObject
 	}
 
 	public static function setClassName($registryClassName = 'Sco_Cookie_Object')
-    {
-        if (self::$_registry !== null) {
-            require_once 'Zend/Exception.php';
-            throw new Zend_Exception('Cookie is already initialized');
-        }
+	{
+		if (self::$_registry !== null)
+		{
+			throw new Exception('Cookie is already initialized');
+		}
 
-        if (!is_string($registryClassName)) {
-            require_once 'Zend/Exception.php';
-            throw new Zend_Exception("Argument is not a class name");
-        }
+		if (!is_string($registryClassName))
+		{
+			throw new Exception("Argument is not a class name");
+		}
 
-        self::$_registryClassName = $registryClassName;
-    }
+		self::$_registryClassName = $registryClassName;
+	}
 
 	/**
 	 * Unset the default registry instance.
@@ -108,10 +94,9 @@ class Sco_Cookie_Object extends ArrayObject
 	 * @return mixed
 	 * @throws Zend_Exception if no entry is registerd for $index.
 	 */
-	public static function get($index)
+	public static function get($index, $default = null)
 	{
-		$instance = self::getInstance();
-		return $instance->offsetGet($index);
+		return self::getInstance()->get($index, $default);
 	}
 
 	/**
@@ -128,18 +113,7 @@ class Sco_Cookie_Object extends ArrayObject
 	 */
 	public static function set($name, $value, $expire = null, $path = null, $domain = null, $secure = null, $httponly = null)
 	{
-		$instance = self::getInstance();
-		$instance->offsetSet($name, $value);
-
-		$instance->_cookies[$name] = array(
-			'name' => $name,
-			'value' => $value,
-			'expire' => $expire,
-			'path' => $path,
-			'domain' => $domain,
-			'secure' => $secure,
-			'httponly' => $httponly,
-			);
+		call_user_func_array(array(self::getInstance(), 'set'), func_get_args());
 	}
 
 	/**
@@ -158,94 +132,19 @@ class Sco_Cookie_Object extends ArrayObject
 		return self::$_registry->offsetExists($index);
 	}
 
-	/**
-	 * @param string $index
-	 * @returns mixed
-	 *
-	 * Workaround for http://bugs.php.net/bug.php?id=40442 (ZF-960).
-	 */
-	public function offsetExists($index)
+	public static function setcookie($name, $value, $expire = null, $path = null, $domain = null, $secure = null, $httponly = null)
 	{
-		return array_key_exists($index, $this);
-	}
-
-	public function __construct($registry = array())
-	{
-		parent::__construct($_COOKIE, Sco_Array::ARRAY_PROP_BOTH);
-
-		foreach ((array)$registry as $k => $v)
+		$args = func_get_args();
+		if ($args[1] === null || $args[2] < 0)
 		{
-			if (method_exists($this, ucfirst($k)))
-			{
-				$this->$k($v);
-			}
+			$args[1] = null;
+			$args[2] = self::EXPIRE_NOW;
 		}
+
+		return call_user_func_array('setcookie', $args);
 	}
 
-	public function setConfig($config)
-	{
-		$this->_config = array_merge($this->_config, $config);
-
-		return $this;
-	}
-
-	public function getConfig($config)
-	{
-		return $this->_config;
-	}
-
-	public function setCookies($cookies)
-	{
-		$this->exchangeArray($cookies);
-
-		return $this;
-	}
-
-	public function getCookies($cookies)
-	{
-		return $this->getArrayCopy();
-	}
-
-	public static function save()
-	{
-		$instance = self::getInstance();
-
-		$map = array(
-			'expire',
-			'path',
-			'domain',
-			'secure',
-			'httponly',
-			);
-
-		foreach ($instance as $name => $value)
-		{
-			$_config = array();
-
-			$_config['name'] = $name;
-			$_config['value'] = $value;
-
-			foreach ($map as $k)
-			{
-				if (isset($instance->_cookies[$name][$k]))
-				{
-					$_config[$k] = $instance->_cookies[$name][$k];
-				}
-				elseif ($k == 'expire' && $instance->_config['default_' . $k] > 0)
-				{
-					$_config[$k] = time() + $instance->_config['default_' . $k];
-				}
-				else
-				{
-					$_config[$k] = $instance->_config['default_' . $k];
-				}
-			}
-
-			self::setcookie($_config);
-		}
-	}
-
-	public static function setcookie($_config)
+	public static function setcookie_array($_config)
 	{
 		if ($_config['value'] === null || $_config['expire'] < 0)
 		{
@@ -260,14 +159,11 @@ class Sco_Cookie_Object extends ArrayObject
 		return setcookie($_config['name'], $_config['value'], (int)$_config['expire'], (string )$_config['path'], (string )$_config['domain'], (bool)$_config['secure'], (bool)$_config['httponly']);
 	}
 
-	public function __destruct()
+	public static function save()
 	{
-		if ($this->_config['autosave'] && $this === self::$_registry)
-		{
-			self::save();
-		}
+		$instance = self::getInstance();
 
-		var_dump(headers_list());
+		return $instance->save();
 	}
 
 }
