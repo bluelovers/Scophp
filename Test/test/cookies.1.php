423a3eb3e0e19a6fcc41dbff2f669ba9b3e5c510
bluelovers <codelovers@users.sourceforge.net>
2012-06-20 09:43:32 +0800

    Sco_Cookie_Helper::header_remove_cookies() (Step: 3)
    Step: 3
    
    Commit ChangedLog:
    
    [M ] modified:
    
    	Test/test/cookies.1.php

--- Test/test/cookies.1.php
+++ Test/test/cookies.1.php
@@ -7,6 +7,10 @@
 
 require_once ('../bootstrap.php');
 
+ob_start();
+
+Sco_Cookie::setClassName('Sco_Cookie_Object');
+
 var_dump($_COOKIE);
 
 Sco_Cookie::set('abc', 123);
@@ -16,8 +20,14 @@ var_dump(Sco_Cookie::get('abc'));
 
 Sco_Cookie::set('abc', null);
 
+setcookie('out', 999);
+
 Sco_Cookie::save();
 
 var_dump($_COOKIE);
 
 var_dump(headers_list(), Sco_Cookie::getInstance());
+
+Sco_Cookie_Helper::header_remove_cookies();
+
+var_dump(headers_list());
