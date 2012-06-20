8a175e58260b9e8c3da9a2b1b898481862f941af
bluelovers <codelovers@users.sourceforge.net>
2012-06-20 10:01:54 +0800

    apache_request_headers()
    Commit ChangedLog:
    
    [A ] new file:
    
    	Scorpio/Syntax/syntax.apache.php

---
+++ Scorpio/Syntax/syntax.apache.php
@@ -0,0 +1,49 @@
+<?php
+
+/**
+ * @author bluelovers
+ * @copyright 2012
+ */
+
+if (!function_exists('apache_response_headers'))
+{
+	/**
+	 * Fetch all HTTP response headers.
+	 * (PHP 4 >= 4.3.0, PHP 5)
+	 *
+	 * @author http://www.php.net/manual/zh/function.apache-response-headers.php#91174
+	 */
+	function apache_response_headers()
+	{
+		$arh = array();
+		$headers = headers_list();
+		foreach ($headers as $header)
+		{
+			$header = explode(HEADER_SEP, $header, 2);
+			$arh[$header[0]] = trim($header[1]);
+		}
+		return $arh;
+	}
+}
+
+if (!function_exists('apache_request_headers'))
+{
+	/**
+	 * Fetches all HTTP request headers from the current request.
+	 * (PHP 4 >= 4.3.0, PHP 5)
+	 *
+	 * @author http://www.php.net/manual/zh/function.apache-request-headers.php#72498
+	 */
+	function apache_request_headers()
+	{
+		foreach ($_SERVER as $key => $value)
+		{
+			if (strpos($key, 'HTTP_') === 0)
+			{
+				$key = str_replace(' ', '-', ucwords(strtolower(str_replace('_', ' ', substr($key, 5)))));
+				$out[$key] = $value;
+			}
+		}
+		return $out;
+	}
+}
