67a25b17093d5395d8d61a414ab22ee2f678cceb
bluelovers <codelovers@users.sourceforge.net>
2012-06-21 05:08:05 +0800

    . (Step: 4)
    Step: 4
    
    Commit ChangedLog:
    
    [M ] modified:
    
    	Test/test/env.2.php

--- Test/test/env.2.php
+++ Test/test/env.2.php
@@ -7,13 +7,34 @@
 
 require_once ('../bootstrap.php');
 
-$_SERVER = new ArrayObject();
+$array_a = new ArrayIterator(array(
+	'a',
+	'b',
+	'c'));
+$array_b = new ArrayIterator(array(
+	'd',
+	'e',
+	'f'));
+$iterator = new AppendIterator;
+$iterator->append($array_a);
+$iterator->append($array_b);
+foreach ($iterator as $current)
+{
+	echo $current . "\n";
+}
+
+exit;
+
+Sco_PHP_Global::init();
+
+$_ENV = new ArrayObject($_ENV, Sco_Array::ARRAY_PROP_BOTH);
 
 function a()
 {
-	$_SERVER['abc'] = 1;
+	//$_SERVER['abc'] = 1;
+	$_ENV['abc'] = 2;
 }
 
 a();
 
-var_dump($_SERVER);
+var_dump($_SERVER, $_ENV, $_GET, $_POST, $GLOBALS);
