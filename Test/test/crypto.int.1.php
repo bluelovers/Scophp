6f29a2111a72931c2ed2ed4c8de810b97d829f85
bluelovers <codelovers@users.sourceforge.net>
2012-06-21 06:44:04 +0800

    .
    Commit ChangedLog:
    
    [A ] new file:
    
    	Test/test/crypto.int.1.php

---
+++ Test/test/crypto.int.1.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * @author bluelovers
+ * @copyright 2012
+ */
+
+require_once ('../bootstrap.php');
+
+$c = new Sco_Crypto_Int();
+
+for ($i = 0; $i < 100; $i++)
+{
+	$e = $c->encode($i);
+	$r = $c->decode($e);
+
+	if ($r !== $i)
+	{
+		var_dump($i, $e, $r);
+	}
+}
+
+$i = '0101';
+
+$e = $c->encode($i);
+$r = $c->decode($e);
+
+var_dump($i, $e, $r);
+
+$e = Sco_Crypto_Int::encode($i);
+$r = Sco_Crypto_Int::decode($e);
+
+var_dump($i, $e, $r);
