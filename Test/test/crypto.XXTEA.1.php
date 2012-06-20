d9ffbd6938d288891f973edc28a916ebc6a41021
bluelovers <codelovers@users.sourceforge.net>
2012-06-21 07:17:26 +0800

    Sco_Crypto_TEA_XXTEA (Step: 2)
    Step: 2
    
    Commit ChangedLog:
    
    [A ] new file:
    
    	Test/test/crypto.XXTEA.1.php

---
+++ Test/test/crypto.XXTEA.1.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * @author bluelovers
+ * @copyright 2012
+ */
+
+require_once ('../bootstrap.php');
+
+$c = new Sco_Crypto_TEA_XXTEA();
+
+for ($i = 0; $i < 100; $i++)
+{
+	$e = $c->encode($i);
+	$r = $c->decode($e);
+
+	if ($r !== (string)$i)
+	{
+		var_dump($i, $e, $r);
+	}
+}$i = '0000101';
+
+$e = $c->encode($i);
+$r = $c->decode($e);
+
+var_dump($i, $e, $r);
+
+$e = Sco_Crypto_TEA_XXTEA::encode($i);
+$r = Sco_Crypto_TEA_XXTEA::decode($e);
+
+var_dump($i, $e, $r);
