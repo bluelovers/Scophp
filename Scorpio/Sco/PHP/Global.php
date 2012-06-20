ac5256f19da2633d91aed6d7e7f22f7387a03d7d
bluelovers <codelovers@users.sourceforge.net>
2012-06-21 05:08:04 +0800

    . (Step: 3)
    Step: 3
    
    Commit ChangedLog:
    
    [M ] modified:
    
    	Scorpio/Sco/PHP/Global.php

--- Scorpio/Sco/PHP/Global.php
+++ Scorpio/Sco/PHP/Global.php
@@ -8,11 +8,28 @@
 class Sco_PHP_Global
 {
 
-	public static $_SERVER;
-
 	public static function init()
 	{
-		self::$_SERVER = $_SERVER;
+		static $init;
+
+		if (isset($init)) return false;
+
+		$_SERVER = new Sco_Array_Iterator_Lockable($_SERVER);
+		$_GET = new Sco_Array_Iterator_Lockable($_GET);
+
+		$_POST = new Sco_Array_Iterator_Lockable($_SERVER['REQUEST_METHOD'] == 'GET' ? array() : $_POST);
+
+		$_SERVER->setName('_SERVER');
+		$_GET->setName('_GET');
+		$_POST->setName('_POST');
+
+		$_SERVER->lock();
+		$_GET->lock();
+		$_POST->lock();
+
+		$GLOBALS = array();
+
+		return $init = true;
 	}
 
 }
