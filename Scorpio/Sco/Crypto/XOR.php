66bf82371ba9b82f6b4d0f2222d2a54128a15246
bluelovers <codelovers@users.sourceforge.net>
2012-06-21 08:14:31 +0800

    . (Step: 1)
    Step: 1
    
    Commit ChangedLog:
    
    [A ] new file:
    
    	Scorpio/Sco/Crypto/XOR.php

---
+++ Scorpio/Sco/Crypto/XOR.php
@@ -0,0 +1,45 @@
+<?php
+
+/**
+ * @author bluelovers
+ * @copyright 2012
+ */
+
+/**
+ * @see http://baike.baidu.com/view/904005.htm
+ */
+class Sco_Crypto_XOR extends Sco_Crypto_Base implements Sco_Crypto_DecodeInterface
+{
+
+	function encode($string)
+	{
+		$string = (string)$string;
+		$key = (string)$this->salt;
+
+		for ($i = 0; $i < strlen($string); $i++)
+		{
+			for ($j = 0; $j < strlen($key); $j++)
+			{
+				$string[$i] = $string[$i] ^ $key[$j];
+			}
+
+		}
+		return $string;
+	}
+
+	function decode($string)
+	{
+		$string = (string)$string;
+		$key = (string)$this->salt;
+
+		for ($i = 0; $i < strlen($string); $i++)
+		{
+			for ($j = 0; $j < strlen($key); $j++)
+			{
+				$string[$i] = $key[$j] ^ $string[$i];
+			}
+		}
+		return $string;
+	}
+
+}
