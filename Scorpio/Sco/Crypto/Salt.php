6637230e09c90f13ba393a82f4e3b908e3a055f5
bluelovers <codelovers@users.sourceforge.net>
2012-06-21 10:23:17 +0800

    . (Step: 5)
    Step: 5
    
    Commit ChangedLog:
    
    [M ] modified:
    
    	Scorpio/Sco/Crypto/Salt.php

--- Scorpio/Sco/Crypto/Salt.php
+++ Scorpio/Sco/Crypto/Salt.php
@@ -18,15 +18,36 @@ class Sco_Crypto_Salt
 		return uniqid($seed, true);
 	}
 
-	public static function random($length, $numeric = 0) {
-		$seed = base_convert(md5(microtime().$_SERVER['DOCUMENT_ROOT']), 16, $numeric ? 10 : 35);
-		$seed = $numeric ? (str_replace('0', '', $seed).'012340567890') : ($seed.'zZ'.strtoupper($seed));
+	public static function random($length = 32, $numeric = 0)
+	{
+		$seed = base_convert(md5(microtime() . $_SERVER['DOCUMENT_ROOT']), 16, $numeric ? 10 : 35);
+		$seed = $numeric ? (str_replace('0', '', $seed) . '012340567890') : ($seed . 'zZ' . strtoupper($seed));
 		$hash = '';
 		$max = strlen($seed) - 1;
-		for($i = 0; $i < $length; $i++) {
+		for ($i = 0; $i < $length; $i++)
+		{
 			$hash .= $seed[mt_rand(0, $max)];
 		}
 		return $hash;
+	}
+
+	public static function word_table($word, $width = 26, $height = null, $format = true, $sep = NL)
+	{
+		if ($height === null)
+		{
+			$height = $width;
+		}
+
+		$len = $width * $height;
+
+		$table = substr(str_repeat($word, ceil($len / strlen($word))), 0, $len);
+
+		if (!$format)
+		{
+			return $table;
+		}
+
+		return wordwrap($table, $width, $sep, true);
 	}
 
 }
