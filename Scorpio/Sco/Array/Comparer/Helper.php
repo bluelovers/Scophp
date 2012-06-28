57f61de9405d67ad26073871bd69ca152b9c9818
bluelovers <codelovers@users.sourceforge.net>
2012-06-28 16:30:48 +0800

    . (Step: 1)
    Step: 1
    
    Commit ChangedLog:
    
    [A ] new file:
    
    	Scorpio/Sco/Array/Comparer/Helper.php

---
+++ Scorpio/Sco/Array/Comparer/Helper.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * @author bluelovers
+ * @copyright 2012
+ */
+
+class Sco_Array_Comparer_Helper
+{
+
+	/**
+	 * @todo
+	 */
+	public function cmpStack($a, $b)
+	{
+		if ($a == $b)
+		{
+			return CMP_KEEP;
+		}
+
+		return ($a < $b) ? CMP_NEXT : CMP_BACK;
+	}
+
+	/**
+	 * @todo this func only maybe work on first time
+	 */
+	public function cmpQueue($a, $b)
+	{
+		if ($a == $b)
+		{
+			return CMP_BACK;
+		}
+
+		return ($a < $b) ? CMP_NEXT : CMP_BACK;
+	}
+
+}
