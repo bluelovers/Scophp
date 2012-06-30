b798331135862d1074243151bb4e9e7b1dcb2d52
bluelovers <codelovers@users.sourceforge.net>
2012-06-30 11:30:26 +0800

    phpunit (Step: 5)
    Step: 5
    
    Commit ChangedLog:
    
    [A ] new file:
    
    	Test/test/file.2.php

---
+++ Test/test/file.2.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * @author bluelovers
+ * @copyright 2012
+ */
+
+require_once ('../bootstrap.php');
+
+var_dump(dirname('../test/.././test/./.\/\/\test.txt'), Sco_File_Format::remove_root('../test/.././test/./.\/\/\test.txt', '..'));
