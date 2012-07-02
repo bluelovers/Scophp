b618ef1cc6c222773e34c71900fee9b791be0fcc
bluelovers <codelovers@users.sourceforge.net>
2012-07-02 23:24:19 +0800

    . (Step: 3)
    Step: 3
    
    Commit ChangedLog:
    
    [M ] modified:
    
    	Test/test/math.1.php

--- Test/test/math.1.php
+++ Test/test/math.1.php
@@ -22,9 +22,30 @@ $array[] = array(-1, -1);
 foreach ($array as $d)
 {
 	printnl(sprintf('(%+d, %+d) == %d', $d[0], $d[1], Sco_Math_Func_Distance::azimuth_compass($d[0], $d[1])));
-	printnl(sprintf('(%+d, %+d) == %d', $d[0], $d[1], Sco_Math_Func_Distance::azimuth_compass2($d[0], $d[1])));
 }
 
+foreach ($array as $d)
+{
+	printnl(sprintf('(%+d, %+d) === %d', $d[0], $d[1], Sco_Math_Func_Distance::azimuth_compass_2($d[0], $d[1])));
+}
+
+foreach ($array as $d)
+{
+	printnl(sprintf('(%-d, %-d) === \'%s\'', $d[0], $d[1], Sco_Math_Func_Distance::polar_2($d[0], $d[1])));
+}
+
+$array = array(
+			'NE' => '22.5',
+			'E' => '67.5',
+			'SE' => '112.5',
+			'S' => '157.5',
+			'SW' => '202.5',
+			'W' => '247.5',
+			'NW' => '292.5',
+			'N' => '337.5',
+			);
+
+var_export(array_flip($array));
 
 function returnRotatedPoint($x,$y,$cx,$cy,$a)
     {
