6cc19b5fe206402933b9635f51c85fd33ed28cc7
bluelovers <codelovers@users.sourceforge.net>
2012-07-03 13:16:47 +0800

    Sco_Ticker_Iterator_Timer (Step: 3)
    Step: 3
    
    Commit ChangedLog:
    
    [M ] modified:
    
    	Scorpio/Sco/Ticker/Iterator/Timer.php

--- Scorpio/Sco/Ticker/Iterator/Timer.php
+++ Scorpio/Sco/Ticker/Iterator/Timer.php
@@ -10,9 +10,80 @@ class Sco_Ticker_Iterator_Timer extends Sco_Ticker_Iterator
 
 	protected $_offsetClass = 'Sco_Ticker_Timer';
 
-	public function timeElapsed($start, $end)
+	public function __construct($data = array())
+	{
+		parent::__construct($data);
+
+		$this->offsetGet('Start');
+	}
+
+	/**
+     * Returns the time elapsed betweens two markers.
+     *
+     * @param string $start start marker, defaults to "Start"
+     * @param string $end   end marker, defaults to "Stop"
+     *
+     * @return double  $time_elapsed time elapsed between $start and $end
+     * @access public
+     */
+	public function timeElapsed($start = 'Start', $end = 'Stop')
 	{
 		return $this[$end]->getTicker() - $this[$start]->getTicker();
 	}
+
+	/**
+     * Set marker.
+     *
+     * @param string $name Name of the marker to be set.
+     *
+     * @see    start(), stop()
+     * @access public
+     * @return void
+     */
+	public function setMarker($name, $timestamp = 'now')
+	{
+		$this[$name]->resetTicker($timestamp);
+
+		return $this;
+	}
+
+	public function toArrayValues($args = array())
+	{
+		if (empty($args))
+		{
+			$args['now'] = self::_getMicrotime();
+		}
+
+		return parent::toArrayValues($args);
+	}
+
+	/**
+     * Set "Start" marker.
+     *
+     * @see    setMarker(), stop()
+     * @access public
+     * @return void
+     */
+    function start()
+    {
+        $this->setMarker('Start');
+    }
+
+    /**
+     * Set "Stop" marker.
+     *
+     * @see    setMarker(), start()
+     * @access public
+     * @return void
+     */
+    function stop()
+    {
+        $this->setMarker('Stop');
+    }
+
+    public function _getMicrotime()
+    {
+        return call_user_func(array($this->_offsetClass, '_getMicrotime'));
+    }
 
 }
