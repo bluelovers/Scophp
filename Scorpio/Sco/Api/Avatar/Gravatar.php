37d4f7c9bac852e02acffe63e3e1e143d5539be1
bluelovers <codelovers@users.sourceforge.net>
2012-07-07 18:23:11 +0800

    .
    Commit ChangedLog:
    
    [M ] modified:
    
    	Scorpio/Sco/Api/Avatar/Gravatar.php

--- Scorpio/Sco/Api/Avatar/Gravatar.php
+++ Scorpio/Sco/Api/Avatar/Gravatar.php
@@ -7,6 +7,10 @@
  * @author http://www.talkphp.com/script-giveaway/1905-gravatar-wrapper-class.html
  */
 
+/**
+ * @see https://en.gravatar.com/site/implement/images/
+ * @see http://en.gravatar.com/site/implement/images/php/
+ */
 class Sco_Api_Avatar_Gravatar implements Sco_Api_Avatar_Interface
 {
 
@@ -16,18 +20,50 @@ class Sco_Api_Avatar_Gravatar implements Sco_Api_Avatar_Interface
 	//const GRAVATAR_SITE_URL = 'avatar.php?gravatar_id=%ssize=%sdefault=%srating=%s&default=%s';
 	const SITE_URL = 'avatar/%s?s=%s&d=%s&r=%s&f=%s';
 
+	const DEFAULT_DEF = '';
+
+	/**
+	 * do not load any image if none is associated with the email hash, instead return an HTTP 404 (File Not Found)
+	 */
 	const DEFAULT_404 = 404;
+	/**
+	 * (mystery-man) a simple, cartoon-style silhouetted outline of a person (does not vary by email hash)
+	 */
 	const DEFAULT_MM = 'mm';
+	/**
+	 * a geometric pattern based on an email hash
+	 */
 	const DEFAULT_IDENTICON = 'identicon';
+	/**
+	 * a generated 'monster' with different colors, faces, etc
+	 */
 	const DEFAULT_MONSTERID = 'monsterid';
+	/**
+	 * generated faces with differing features and backgrounds
+	 */
 	const DEFAULT_WAVATAR = 'wavatar';
+	/**
+	 * awesome generated, 8-bit arcade-style pixelated faces
+	 */
 	const DEFAULT_RETRO = 'retro';
 
 	const SIZE_80 = 80;
 
+	/**
+	 * suitable for display on all websites with any audience type.
+	 */
 	const RATING_G = 'g';
+	/**
+	 * may contain rude gestures, provocatively dressed individuals, the lesser swear words, or mild violence.
+	 */
 	const RATING_PG = 'pg';
+	/**
+	 * may contain such things as harsh profanity, intense violence, nudity, or hard drug use.
+	 */
 	const RATING_R = 'r';
+	/**
+	 * may contain hardcore sexual imagery or extremely disturbing violence.
+	 */
 	const RATING_X = 'x';
 
 	const YES = 'y';
@@ -36,7 +72,7 @@ class Sco_Api_Avatar_Gravatar implements Sco_Api_Avatar_Interface
 	protected $m_szEmail;
 
 	protected $m_iSize = self::SIZE_80;
-	protected $m_szImage = self::DEFAULT_MM;
+	protected $m_szImage = self::DEFAULT_DEF;
 	protected $m_szRating = self::RATING_G;
 
 	protected $m_force_default = self::NO;
@@ -68,6 +104,13 @@ class Sco_Api_Avatar_Gravatar implements Sco_Api_Avatar_Interface
 		return $this;
 	}
 
+	/**
+	 * What happens when an email address has no matching Gravatar image
+	 *
+	 * If you'd prefer to use your own default image (perhaps your logo, a funny face, whatever)
+	 * , then you can easily do so by supplying the URL to an image in the d= or default= parameter.
+	 * The URL should be URL-encoded to ensure that it carries across correctly
+	 */
 	public function setDefaultImage($szImage)
 	{
 		$this->m_szImage = urlencode($szImage);
@@ -83,6 +126,12 @@ class Sco_Api_Avatar_Gravatar implements Sco_Api_Avatar_Interface
 	public function setDefaultImageAs404()
 	{
 		$this->m_szEmail = self::DEFAULT_404;
+		return $this;
+	}
+
+	public function setDefaultImageAsDefault()
+	{
+		$this->m_szEmail = self::DEFAULT_DEF;
 		return $this;
 	}
 
@@ -122,6 +171,12 @@ class Sco_Api_Avatar_Gravatar implements Sco_Api_Avatar_Interface
 		return $this;
 	}
 
+	/**
+	 * By default, images are presented at 80px by 80px if no size parameter is supplied.
+	 * You may request a specific image size
+	 * , which will be dynamically delivered from Gravatar by using the s= or size= parameter
+	 * and passing a single pixel dimension (since the images are square):
+	 */
 	public function setSize($iSize)
 	{
 		$this->m_iSize = urlencode((int)$iSize ? (int)$iSize : self::SIZE_80);
